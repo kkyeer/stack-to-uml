ec6d0b27ec9ca985adb3410fecc186d628ca2732
kkyeer <kkyeer@gmail.com>
2022-12-21 21:53:41 +0800

    feat: add save to file

--- src/main/java/com/kkyeer/stack/to/uml/core/disp/SVGDisplayPanel.java
+++ src/main/java/com/kkyeer/stack/to/uml/core/disp/SVGDisplayPanel.java
@@ -1,10 +1,14 @@
 package com.kkyeer.stack.to.uml.core.disp;
 
 import org.apache.batik.swing.JSVGCanvas;
+import org.apache.commons.io.FileUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @Author: kkyeer
@@ -15,6 +19,12 @@ import java.io.File;
 public class SVGDisplayPanel {
     private File imgFile;
 
+    private JSVGCanvas canvas;
+
+    private static JFrame frame;
+
+
+
     public SVGDisplayPanel(File imgFile) {
         this.imgFile = imgFile;
     }
@@ -22,28 +32,72 @@ public class SVGDisplayPanel {
     public JComponent createPanel(){
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout(0, 0));
-        panel.setSize(20,20);
-
-        // JLabel imgLabel = new JLabel(new ImageIcon("path_to_image.png"));
-        // panel.add(imgLabel, -1);
-        // JTextArea jTextArea = new JTextArea();
-        // // jTextArea.setSize(30,30);
-        // panel.add(jTextArea,BorderLayout.CENTER);
-        JLabel imgLabel = new JLabel(new ImageIcon(this.imgFile.getAbsolutePath()));
-        imgLabel.setSize(20,20);
-        panel.add(imgLabel,BorderLayout.CENTER);
-
+        panel.setSize(400,400);
         JSVGCanvas jsvgCanvas = new JSVGCanvas();
+        jsvgCanvas.setSize(200,200);
+
         jsvgCanvas.setURI(imgFile.toURI().toString());
+        this.canvas = jsvgCanvas;
+        // jsvgCanvas.getInteractors().clear();
+        // jsvgCanvas.getInteractors().add(new ZoomInInteraction());
         panel.add(jsvgCanvas, BorderLayout.CENTER);
+
+
+
+        JMenuBar menuBar = new JMenuBar();
+        configureMenuBar(menuBar);
+        panel.add(menuBar, BorderLayout.NORTH);
         return panel;
+    }
+
+    private void configureMenuBar(JMenuBar menuBar) {
+        JButton saveBtn = new JButton();
+        saveBtn.setText("Save");
+        saveBtn.setVisible(true);
+        saveBtn.addActionListener(
+                e -> {
+                    configureFileChooser(frame);
+                }
+        );
+        menuBar.add(saveBtn);
+
+        JButton resetZoomBtn = new JButton();
+        resetZoomBtn.setText("ResetZoom");
+        resetZoomBtn.setVisible(true);
+        AtomicReference<JSVGCanvas> canvasRef = new AtomicReference<>(this.canvas);
+        resetZoomBtn.addActionListener(
+                e ->{
+                    AffineTransform initialTransform = canvasRef.get().getInitialTransform();
+                    // paintingTransform.setToScale(1.0, 1.0);
+                    canvasRef.get().setPaintingTransform(initialTransform);
+                }
+        );
+        menuBar.add(resetZoomBtn);
+    }
+
+
+    private void configureFileChooser(Component parent) {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jFileChooser.setVisible(true);
+        jFileChooser.setCurrentDirectory(new File("./"));
+        int code = jFileChooser.showSaveDialog(parent);
+        if (JFileChooser.APPROVE_OPTION == code) {
+            File selectedFile = jFileChooser.getSelectedFile();
+            try {
+                FileUtils.copyFile(imgFile, selectedFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void show(){
         JFrame jFrame = new JFrame();
-        jFrame.setLayout(new BorderLayout());
-        jFrame.setSize(300,300);
-        jFrame.add(createPanel(),BorderLayout.CENTER);
+        SVGDisplayPanel.frame = jFrame;
+        jFrame.setSize(1000,1000);
+        jFrame.getContentPane().add(createPanel());
         jFrame.setVisible(true);
+
     }
 }
